968e7b17edab4a80aebb32f9c2153f12ed4529b9
vivek.sharma <vivek.sharma@silvergrey.in>
2026-09-08 22:14:11 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -2,12 +2,25 @@ const express=require('express');
 
 const app= express();
 
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Vivke",latName:"sharma"});
+});
+app.post("/user",(req,res)=>{
+
+    res.send("Data save in db successfully!");
+});
+
+app.delete("/user",(req,res)=>{
+
+    res.send("Data delete from db successfully!");
+});
 app.use("/hello",(req,res)=>{
     res.send("Hello hello!");
 });
 app.use("/test",(req,res)=>{
     res.send("Hello from server!");
 });
+
 app.listen(3000,()=>{
     console.log("server is successfully listen port 3000");
 });
